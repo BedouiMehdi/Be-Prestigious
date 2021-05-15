8117609ac799fefbd974269aa9f5c2876f6f530f
aminekhemili <61553337+aminekhemili@users.noreply.github.com>
2021-05-15 08:50:07 +0100

    Add files via upload

---
+++ datax.php
@@ -0,0 +1,51 @@
+<?php
+  class ccontact_us {
+
+    private String Name;
+    private int Mobile_num;
+    private String Email;
+    private String message;
+      // Getter
+  public String getName() {
+    return Name;
+  }
+
+  // Setter
+  public void setName(String newName) {
+    this.Name = newName;
+  }
+   // Getter
+  public int getMobile_num() {
+    return Mobile_num;
+  }
+
+  // Setter
+  public void setMobile_num(int newMobile_num) {
+    this.Mobile_num = newMobile_num;
+  }
+  // Getter
+  public String getEmail() {
+    return Email;
+  }
+
+  // Setter
+  public void setEmail(String newEmail) {
+    this.Email = newEmail;
+  }
+  // Getter
+  public String getmessage() {
+    return message;
+  }
+
+  // Setter
+  public void setmessage(String newmessage) {
+    this.message = newmessage;
+  }
+ function construct ($Name,$Mobile_num,$Email,$message)
+ $this.Name=Name;
+ $this.Mobile_num=Mobile_num.
+ $this.Email=Email;
+ $this.message=message;
+ 
+  }
+?>
